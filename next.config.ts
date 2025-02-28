7f1b15132d3f195bed5e7a41aa0abf9631eefa78
Sirtheprogrammer <sirtheprogrammer@gmail.com>
2025-02-28 22:56:45 +0300

    chore: Update deployment configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          publicPath: '/_next/static/media/',
+          outputPath: 'static/media/',
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
